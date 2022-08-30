5facb129b7fa48a78015800635916cbf7af5110d
qu3n71n <54708262+qu3n71n@users.noreply.github.com>
2022-08-30 10:32:22 +0800

    Update organize_files.sh

--- openwrt-21.02/steps/organize_files.sh
+++ openwrt-21.02/steps/organize_files.sh
@@ -15,20 +15,20 @@ RELTAG=$4
 
 mkdir -p ./artifact/
 mv build/openwrt/bin/targets/rockchip/armv8/* ./artifact/
-mv build/openwrt/bin/targets/rockchip/armv8/*.buildinfo ./artifact/
-mv build/openwrt/bin/targets/rockchip/armv8/*sysupgrade.img* ./artifact/
+#mv build/openwrt/bin/targets/rockchip/armv8/*.buildinfo ./artifact/
+#mv build/openwrt/bin/targets/rockchip/armv8/*sysupgrade.img* ./artifact/
 
 cd ./artifact/
 #ls -Ahl
-mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-ext4-sysupgrade.img.gz OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-ext4.img.gz
-mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-squashfs-sysupgrade.img.gz OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-squashfs.img.gz
-gzip -d *.gz && exit 0
-gzip --best *.img
+#mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-ext4-sysupgrade.img.gz OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-ext4.img.gz
+#mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-squashfs-sysupgrade.img.gz OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-squashfs.img.gz
+#gzip -d *.gz && exit 0
+#gzip --best *.img
 ls -Ahl
 zip -r OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG.zip ./
-zip OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-buildinfo.zip *.buildinfo
-zip OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-ext4.zip *$VARIANT*ext4*
-zip OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-squashfs.zip *$VARIANT*squashfs*
+#zip OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-buildinfo.zip *.buildinfo
+#zip OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-ext4.zip *$VARIANT*ext4*
+#zip OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-squashfs.zip *$VARIANT*squashfs*
 #cp ../build/openwrt/*.config ./
 ls -Ahl
 
