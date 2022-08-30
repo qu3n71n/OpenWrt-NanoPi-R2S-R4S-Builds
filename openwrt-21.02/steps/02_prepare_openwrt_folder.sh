3076946ee136b1476ab2c87ef38225a6d6de400f
qu3n71n <54708262+qu3n71n@users.noreply.github.com>
2022-08-30 14:45:17 +0800

    remove kenzok8 feeds

--- openwrt-21.02/steps/02_prepare_openwrt_folder.sh
+++ openwrt-21.02/steps/02_prepare_openwrt_folder.sh
@@ -17,8 +17,8 @@ cd openwrt
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # add custom feeds
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git luciImmortalWrt https://github.com/immortalwrt/luci.git;openwrt-21.02' feeds.conf.default
 sed -i '$a src-git packagesImmortalWrt https://github.com/immortalwrt/packages.git;openwrt-21.02' feeds.conf.default
 git pull
