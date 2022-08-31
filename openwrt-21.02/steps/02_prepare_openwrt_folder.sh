c2d5b99222760ad45c14b1b5dc4989be5cf2d58a
qu3n71n <54708262+qu3n71n@users.noreply.github.com>
2022-08-31 20:08:50 +0800

    Update 02_prepare_openwrt_folder.sh

--- openwrt-21.02/steps/02_prepare_openwrt_folder.sh
+++ openwrt-21.02/steps/02_prepare_openwrt_folder.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-ROOTDIR=$(pwd)
-echo $ROOTDIR
-if [ ! -e "$ROOTDIR/build" ]; then
+USERDIR=$(pwd)
+echo $USERDIR
+if [ ! -e "$USERDIR/build" ]; then
     echo "Please run from root / no build dir"
     exit 1
 fi
 
-cd "$ROOTDIR/build"
+cd "$USERDIR/build"
 
 cp -R openwrt-fresh-2102 openwrt
 
@@ -16,11 +16,13 @@ cd openwrt
 # modify default ip
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
+# modify default feeds
+
 # add custom feeds
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git luciImmortalWrt https://github.com/immortalwrt/luci.git;openwrt-21.02' feeds.conf.default
-sed -i '$a src-git packagesImmortalWrt https://github.com/immortalwrt/packages.git;openwrt-21.02' feeds.conf.default
+sed -i 's,https://git.openwrt.org/feed/packages.git;openwrt-21.02,https://github.com/immortalwrt/luci.git;openwrt-21.02,g' feeds.conf.default
+sed -i 's,https://git.openwrt.org/project/luci.git;openwrt-21.02,https://github.com/immortalwrt/packages.git;openwrt-21.02,g' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git pull
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
